5b83fb3c2e6be9523c0190a4287ead135b6720e7
root <root@DESKTOP-3S5SIJL.localdomain>
2024-02-06 12:14:11 +0300

    feat:fetch random image

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, {useState, useEffect} from 'react'
+
+const App = () => {
+  const [dogImg, setDogImg] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() =>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) =>{
+      setDogImg(data.message)
+      setIsLoaded(true);
+    })
+  })
+
+  if (!isLoaded) return <p>Loading...</p>;
+  return (
+    <div>
+      <img src={dogImg} alt="A Random Dog" />
+    </div>
+  )
+}
+
+export default App
